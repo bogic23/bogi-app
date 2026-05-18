a402a734ef92d333ca31a70c0d424933257486bd
bogic23 <bogichristian23@gmail.com>
2026-05-18 12:02:18 +0700

    add firebase authentication, page login/register, (unfinished) money manager

--- app/src/androidTest/java/com/abc/personaldashboard/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/abc/personaldashboard/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.abc.personaldashboard
+package com.abc.locusvisionis
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.abc.personaldashboard", appContext.packageName)
+        assertEquals("com.abc.locusvisionis", appContext.packageName)
     }
 }
